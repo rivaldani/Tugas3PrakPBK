4e286299e00cd4193e13c8b9759d80d7fbbe97e8
rivaldani <muhammadrivaldani@student.uir.ac.id>
2024-05-15 18:18:11 +0700

    commit pertama

--- src/App.vue
+++ src/App.vue
@@ -2,59 +2,6 @@
 import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-
-// State
-const showPostForm = ref(false)
-const newPost = ref({
-  title: '',
-  body: ''
-})
-const selectedUser = ref('')
-const users = ref([])
-
-// Fetch users from JSONPlaceholder
-const fetchUsers = async () => {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    if (response.ok) {
-      const data = await response.json()
-      users.value = data
-    } else {
-      console.error('Failed to fetch users')
-    }
-  } catch (error) {
-    console.error('Error fetching users:', error)
-  }
-}
-fetchUsers()
-
-// Submit post
-const submitPost = async () => {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        ...newPost.value,
-        userId: selectedUser.value // Menambahkan userId ke dalam data post
-      })
-    })
-
-    if (response.ok) {
-      console.log('Post submitted successfully')
-      newPost.value.title = ''
-      newPost.value.body = ''
-      selectedUser.value = ''
-      showPostForm.value = false
-    } else {
-      console.error('Failed to submit post')
-    }
-  } catch (error) {
-    console.error('Error submitting post:', error)
-  }
-}
 </script>
 
 <template>
@@ -67,28 +14,11 @@ const submitPost = async () => {
       <nav>
         <RouterLink to="/">Todolist</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <button @click="showPostForm = true">Post</button>
-        <select v-model="selectedUser">
-          <option disabled value="">Select a user</option>
-          <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
-        </select>
       </nav>
     </div>
   </header>
 
   <RouterView />
-  <div v-if="showPostForm">
-    <h2>Create a new post</h2>
-    <form @submit.prevent="submitPost">
-      <label for="title">Title:</label><br>
-      <input type="text" id="title" v-model="newPost.title" required><br>
-
-      <label for="body">Body:</label><br>
-      <textarea id="body" v-model="newPost.body" required></textarea><br>
-
-      <button type="submit">Submit</button>
-    </form>
-  </div>
 </template>
 
 <style scoped>
